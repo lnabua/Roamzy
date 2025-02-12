7c29a4920a8e5ae120607fff65a9ad5dfd20641e
Lorraine Nabua <lorrainenabua@Lorraines-MacBook-Air.local>
2025-02-12 11:46:29 -0800

    Created vehicle card and updated vehicle list api

---
+++ frontend/src/components/VehicleCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, CardActionArea, CardMedia, CardContent, Typography } from '@mui/material';
+
+const VehicleCard = ({ name, price, image }) => {
+    return (
+        <Card sx={{ maxWidth: 300 }}>
+        <CardActionArea>
+            <CardMedia
+            component="img"
+            height="200"
+            image={image}
+            />
+            <CardContent>
+            <Typography gutterBottom variant="body1" component="div">
+                {name}
+            </Typography>
+            <Typography variant="body2">
+                {price}/night
+            </Typography>
+            </CardContent>
+        </CardActionArea>
+        </Card>
+    )
+}
+
+export default VehicleCard;
--- frontend/src/components/VehicleList.jsx
+++ frontend/src/components/VehicleList.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { getVehicles } from "../api/vehicleApi";
-import { Container } from "@mui/material";
+import { Box, Container, CircularProgress, Alert, Typography, Grid } from "@mui/material";
+import VehicleCard from "./VehicleCard";
 
 const VehicleList = () => {
     const [vehicles, setVehicles] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchVehicles = async () => {
@@ -13,23 +15,32 @@ const VehicleList = () => {
                 setVehicles(response.data);
                 setLoading(false);
             } catch (error) {
-                console.error('Error fetching vehicles');
+                setError("Error fetching vehicles");
                 setLoading(false);
             }
         };
         fetchVehicles();
     }, []);
 
-    if (loading) return <p>Loading...</p>;
+
+    if (loading) return <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}><CircularProgress /></Box>;
+
+    if (error) return <Alert severity="error">{error}</Alert>;
 
     return (
         <Container>
-            <h2>Vehicle List</h2>
-            <ul>
+            <Typography variant="h4" gutterBottom>Vehicle List</Typography>
+            <Grid container spacing={4}>
                 {vehicles.map((vehicle) => (
-                    <li key={vehicle._id}>{vehicle.name}</li>
+                <Grid item key={vehicle._id} xs={12} sm={6} md={4}>
+                    <VehicleCard
+                    name={vehicle.name}
+                    price={vehicle.price}
+                    image={vehicle.image}
+                    />
+                </Grid>
                 ))}
-            </ul>   
+            </Grid>
         </Container>
     );
 };
